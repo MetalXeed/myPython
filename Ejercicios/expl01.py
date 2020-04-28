565050a5fe09c90769bbbe9bc81a59ab0d195574
MetalXeed <52155558+MetalXeed@users.noreply.github.com>
2020-04-28 22:18:14 +0200

    corrected program

--- Ejercicios/expl01.py
+++ Ejercicios/expl01.py
@@ -5,17 +5,17 @@ NAME = input('ESCRIBE TU NOMBRE: ')
 print('Tu nombre es: ' + NAME)
 
 """
-ftax=1.20
+ftax=0.20
 sdeduc=10000.00
 adepen=2000.00
 gincome=input('Enter the gross income: ')
 ndep=input('Enter the number of dependencies: ')
 fndep=float(ndep)
 
-itax=gincome*ftax
-itax=itax-sdeduc
-itax=itax-sdeduc*fndep
+
+tax=gicome-sdeduc-adepen*fndep
+ftax=gincome*itax
 """
 itax=(gincome*ftax-sdeduc-sdeduc*fndep)
 """
-print('The income tax is: '+ itax)
+print('The income tax is: '+ ftax)
