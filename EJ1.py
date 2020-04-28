84454e8a249d4af06846b184e7a77992f4648ee6
MetalXeed <52155558+MetalXeed@users.noreply.github.com>
2020-04-28 20:50:56 +0200

    Create EJ1.py

---
+++ EJ1.py
@@ -0,0 +1,9 @@
+#xh = input("Enter Hours: ")
+#xr = input("Enter Rate:" )
+#xp = float(xh) * float(xr)
+#print("Pay: ", xp)
+x = 15
+if x < 10:
+    print(" x is less than ten")
+else:
+     print("x is not less than ten")
